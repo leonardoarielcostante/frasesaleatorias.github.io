ad2d8b453691cf312949c35b548c13632349ad95
leonardoarielcostante <leonardoarielcostante@gmail.com>
2023-01-26 02:27:47 -0300

    Se agrego el link de twitter correctamente

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   const [dataFrases, setFrases] = useState('');
   const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+  const twitter = "https://www.twitter.com/intent/tweet?text=" + dataFrases.quote;
 
   const obtenerDatos = () => {
     fetch(url)
@@ -28,7 +29,7 @@ function App() {
           </div>
           <div className='botones'>
           <button id='new-quote' onClick={ obtenerDatos }>Frase!</button>
-          <a href='https://www.twitter.com/intent/tweet' id='tweet-quote'>Tweet!</a>
+          <a href={twitter} id='tweet-quote'>Tweet!</a>
           </div>
         </div>
     </div>
